7d04eec93892eaf6f36d8bb9dbedc47531103305
Arunlal <arunlal.m2000@gmail.com>
2026-07-27 14:30:00 +0530

    style(navigation): refine navbar positioning and social icons styling

--- Portfolio-Website/src/components/SocialIcons.tsx
+++ Portfolio-Website/src/components/SocialIcons.tsx
@@ -80,7 +80,7 @@ const SocialIcons = () => {
           </a>
         </span>
       </div>
-      <a className="resume-button" href="#">
+      <a className="resume-button" data-cursor="icons" href="#">
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
