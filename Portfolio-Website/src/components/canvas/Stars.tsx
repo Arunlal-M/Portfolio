c7314638961925d47d069ab7b8d0fc2bfe5ccd28
Arunlal <arunlal.m2000@gmail.com>
2026-08-30 13:30:00 +0530

    feat: add new canvas components (Earth and Stars)

---
+++ Portfolio-Website/src/components/canvas/Stars.tsx
@@ -0,0 +1,48 @@
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+// @ts-ignore
+import * as random from "maath/random/dist/maath-random.esm";
+import type { Group } from "three";
+
+const Stars = (props: any) => {
+  const ref = useRef<Group>(null);
+  const [sphere] = useState(() => random.inSphere(new Float32Array(1000), { radius: 1.2 }) as Float32Array);
+
+  useFrame((_, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 15;
+    }
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color='#39FF14'
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
+
+const StarsCanvas = () => {
+  return (
+    <div style={{ width: '100%', height: '100%', position: 'absolute', inset: 0, zIndex: -1 }}>
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarsCanvas;
