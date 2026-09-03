8438f4418cd94859d20fa058db5e22c9f474c05d
Arunlal <arunlal.m2000@gmail.com>
2026-09-03 10:00:00 +0530

    Add progress utility

---
+++ Portfolio-Website/src/utils/progress.ts
@@ -0,0 +1,39 @@
+export const setProgress = (setLoading: (value: number) => void) => {
+  let percent: number = 0;
+  let interval: ReturnType<typeof setInterval> = setInterval(() => {
+    if (percent <= 50) {
+      const rand = Math.round(Math.random() * 5);
+      percent = percent + rand;
+      setLoading(percent);
+    } else {
+      clearInterval(interval);
+      interval = setInterval(() => {
+        percent = percent + Math.round(Math.random());
+        setLoading(percent);
+        if (percent > 91) {
+          clearInterval(interval);
+        }
+      }, 2000);
+    }
+  }, 100);
+
+  function clear() {
+    clearInterval(interval);
+  }
+
+  function loaded() {
+    return new Promise<number>((resolve) => {
+      clearInterval(interval);
+      interval = setInterval(() => {
+        if (percent < 100) {
+          percent++;
+          setLoading(percent);
+        } else {
+          resolve(percent);
+          clearInterval(interval);
+        }
+      }, 2);
+    });
+  }
+  return { loaded, percent, clear };
+};
