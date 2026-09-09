128b42f84a6638dbc235c4513c6d6f2dcf0570e5
Arunlal <arunlal.m2000@gmail.com>
2026-09-09 13:00:00 +0530

    Update Navbar and Social Icons UI

--- Portfolio-Website/src/components/SocialIcons.tsx
+++ Portfolio-Website/src/components/SocialIcons.tsx
@@ -80,7 +80,7 @@ const SocialIcons = () => {
           </a>
         </span>
       </div>
-      <a className="resume-button" data-cursor="icons" href="#">
+      <a className="resume-button" data-cursor="icons" href="/Arunlal_Resume.pdf" target="_blank" rel="noopener noreferrer">
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
